5414ae2fc438cf9ac24007be72add8b4eb7f17b1
Dickson <dicksonchow2007@gmail.com>
2016-01-18 01:06:15 +0800

    Apply PCA in a correct steps and finally get some positive result. Need to apply LDA next.

--- pca/cos_similarity.py
+++ pca/cos_similarity.py
@@ -5,6 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from database import connect_to_database
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from scipy import spatial
 
 
@@ -23,89 +24,66 @@ def get_hausdorff_distance(lA, lB):
     return np.average(min_distance)
 
 
-def get_normalized_data(SELECT_QUERY):
+def get_features_from_database(SELECT_QUERY):
+    # for x in novel:
+    #     del x[-1]
     rows = [item['feature_value'] for item in connect_to_database.execute_select_query(SELECT_QUERY)]
-    novel = [rows[x:x + 57] for x in xrange(0, len(rows), 57)]
+    return [rows[x:x + 57] for x in xrange(0, len(rows), 57)]
 
-    mean = sum(x)/len(x)
-    std_dev = (1/len(x) * sum([ (x_i - mean)**2 for x_i in x]))**0.5
 
-    z_scores = [(x_i - mean)/std_dev for x_i in x]
+def get_normalized_data(features):
+    # mean = sum(x)/len(x)
+    # std_dev = (1/len(x) * sum([ (x_i - mean)**2 for x_i in x]))**0.5
+    # z_scores = [(x_i - mean)/std_dev for x_i in x]
+    X = np.array(features)
+    return preprocessing.scale(X)
 
-    X = np.array(novel)
+
+def PCA_reduce_dimensionality(features):
     # only 2 components are retained
-    return preprocessing.normalize(X)
+    # X_norm = preprocessing.StandardScaler().fit_transform(X)
+    X = get_normalized_data(features)
+    pca = PCA(n_components=3)
+    return pca.fit_transform(X)
 
+def LDA_reduce_dimenstionality(features):
+    X = get_normalized_data(feature_list)
+    lda = LDA(n_components=3)
+    return lda.fit_transform(X)
 
+author_list = []
+feature_list = []
 
-def reduce_dimensionality(SELECT_QUERY):
-    rows = [item['feature_value'] for item in connect_to_database.execute_select_query(SELECT_QUERY)]
-    novel = [rows[x:x + 57] for x in xrange(0, len(rows), 57)]
-    for x in novel:
-        del x[-1]
+SQL_1 = "SELECT feature_value FROM fact WHERE doc_id = 1 ORDER BY para_id, feature_id;" #blue
+t1 = get_features_from_database(SQL_1)
+feature_list.extend(t1)
+author_list.extend([0 for x in range(len(t1))])
 
-    X = np.array(novel)
-    # only 2 components are retained
-    X_norm = preprocessing.normalize(X)
-    pca_1 = PCA(n_components=2)
-    return pca_1.fit_transform(X_norm)
+SQL_2 = "SELECT feature_value FROM fact WHERE doc_id = 408 ORDER BY para_id, feature_id;" #green
+t2 = get_features_from_database(SQL_2)
+feature_list.extend(t2)
+author_list.extend([1 for x in range(len(t2))])
 
-#     # #cur.execute("SELECT feature_value FROM fact WHERE doc_id = 9 AND para_id = 1617 AND (feature_id = 1 OR feature_id = 2 OR feature_id = 3 OR feature_id = 4 OR feature_id = 5 OR feature_id = 16 OR feature_id = 29 OR feature_id = 30);") #add ordered by feature_id
-#     # cur.execute("SELECT feature_value FROM fact WHERE doc_id = 1 AND para_id = 10;") #add ordered by feature_id
+SQL_3 = "SELECT feature_value FROM fact WHERE doc_id = 318 ORDER BY para_id, feature_id;" #red
+t3 = get_features_from_database(SQL_3)
+feature_list.extend(t3)
+author_list.extend([2 for x in range(len(t3))])
 
-#     # #cur.execute("SELECT feature_value FROM fact WHERE doc_id = 9 AND para_id = 1620 AND (feature_id = 1 OR feature_id = 2 OR feature_id = 3 OR feature_id = 4 OR feature_id = 5 OR feature_id = 16 OR feature_id = 29 OR feature_id = 30);")
-#     # cur.execute("SELECT feature_value FROM fact WHERE doc_id = 2 AND para_id = 500;")
+fig = plt.figure(1, figsize=(4, 3))
 
-#     # cur.execute("SELECT feature_value FROM fact WHERE doc_id = 11 AND para_id = 1771 AND (feature_id = 1 OR feature_id = 2 OR feature_id = 3 OR feature_id = 4 OR feature_id = 5 OR feature_id = 16 OR feature_id = 29 OR feature_id = 30);")
+X = PCA_reduce_dimensionality(feature_list)
+y = np.choose(author_list, [0, 1, 2]).astype(np.float)
 
-#     # #cur.execute("SELECT feature_value FROM fact WHERE doc_id = 1 AND (feature_id = 1 OR feature_id = 2 OR feature_id = 3 OR feature_id = 4 OR feature_id = 5 OR feature_id = 16 OR feature_id = 29 OR feature_id = 30);")
-#     # cur.execute("SELECT feature_value FROM fact WHERE doc_id = 1 ORDER BY para_id;")
+plt.clf()
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+plt.cla()
+ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y)
+ax.set_xlabel('PCA1')
+ax.set_ylabel('PCA2')
+ax.set_zlabel('PCA3')
 
-#     #cur.execute("SELECT feature_value FROM fact WHERE doc_id = 2 AND (feature_id = 1 OR feature_id = 2 OR feature_id = 3 OR feature_id = 4 OR feature_id = 5 OR feature_id = 16 OR feature_id = 29 OR feature_id = 30);")
-#     cur.execute("SELECT feature_value FROM fact WHERE doc_id = 1 ORDER BY para_id;")
-
-#     # print get_hausdorff_distance(l4, l5)
-#     # print spatial.distance.euclidean(l1, l2)
-#     # print spatial.distance.cosine(l1, l2)
-
-SQL_1 = "SELECT feature_value FROM fact WHERE doc_id in (SELECT doc_id FROM document WHERE author_id = 4) ORDER BY para_id, feature_id;"
-t1 = np.array(reduce_dimensionality(SQL_1))
-
-SQL_2 = "SELECT feature_value FROM fact WHERE doc_id in (SELECT doc_id FROM document WHERE author_id = 5) ORDER BY para_id, feature_id;"
-t2 = np.array(reduce_dimensionality(SQL_2))
-
-SQL_3 = "SELECT feature_value FROM fact WHERE doc_id in (SELECT doc_id FROM document WHERE author_id = 6) ORDER BY para_id, feature_id;"
-t3 = np.array(reduce_dimensionality(SQL_3))
-
-SQL_4 = "SELECT feature_value FROM fact WHERE doc_id = 636"
-t4 = np.array(get_normalized_data(SQL_4))
-
-SQL_5 = "SELECT feature_value FROM fact WHERE doc_id = 2288"
-t5 = np.array(get_normalized_data(SQL_5))
-
-SQL_6 = "SELECT feature_value FROM fact WHERE doc_id = 1581"
-t6 = np.array(get_normalized_data(SQL_6))
-
-SQL_7 = "SELECT feature_value FROM fact WHERE doc_id = 176"
-
-
-# print get_hausdorff_distance(t4, t5)
-# print get_hausdorff_distance(t4, t6)
-
-fig = plt.figure()
-
-plt.scatter(t4[:, 0], t4[:, 1],  marker='x', color='r')
-plt.scatter(t5[:, 0], t5[:, 1],  marker='^', color='g')
-plt.scatter(t6[:, 0], t6[:, 1],  marker='o', color='b')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.autoscale(enable=True, axis=u'both', tight=False)
-
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(t4[:, 0], t4[:, 1], t4[:, 2], marker='^', color='r')
-# ax.scatter(t5[:, 0], t5[:, 1], t5[:, 2], marker='o', color='g')
-# ax.scatter(t6[:, 0], t6[:, 1], t6[:, 2], marker='x', color='b')
-# ax.set_xlabel('PCA1')
-# ax.set_ylabel('PCA2')
-# ax.set_zlabel('PCA3')
+# plt.scatter(X[:, 0], X[:, 1], c=y)
+# plt.xlabel('PC1')
+# plt.ylabel('PC2')
+# plt.autoscale(enable=True, axis=u'both', tight=False)
 plt.show()
