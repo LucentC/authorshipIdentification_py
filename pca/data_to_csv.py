aef7731384fa04b85bb1d350768dfb17c6a70323
Dickson <dicksonchow2007@gmail.com>
2016-02-28 03:28:22 +0800

    add function to extract author...feature 57 and transform it to a csv file

--- pca/data_to_csv.py
+++ pca/data_to_csv.py
@@ -8,5 +8,6 @@ with open('output.csv', 'wb') as csvfile:
     csv_wr.writerow(header_row)
 
     # 2 refers to document ID, now this function only support getting data with doc_id
+    # please change the doc_id in the following function
     for row in data_analysis.output_author_features_lists(2):
         csv_wr.writerow(row)
