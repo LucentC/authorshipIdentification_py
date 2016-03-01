ade31c92af83f0a8428cc8f576dd4abf0adb3cd4
Dickson <dicksonchow2007@gmail.com>
2016-03-02 03:28:24 +0800

    updated the crawler

--- gutenberg_crawler/GutenbergSpider.py
+++ gutenberg_crawler/GutenbergSpider.py
@@ -3,42 +3,59 @@ import time
 from BookItem import BookItem
 from scrapy.crawler import CrawlerProcess
 from scrapy.http.request import Request
+from scrapy.pipelines.files import FilesPipeline
 from bs4 import BeautifulSoup
-import data_etl.plaintext_data_etl
+#import data_etl.plaintext_data_etl
 
 
 class GutenbergSpider(scrapy.Spider):
     name = "gutenberg"
-    allowed_domains = ["gutenberg.org"]
+    allowed_domains = ["prj61.cs.cityu.edu.hk"]
     start_urls = [
         "http://prj61.cs.cityu.edu.hk/"
     ]
 
     def parse(self, response):
-        for latter_url in response.xpath('//ul[@class="browser alpha"]/li/@href').extract():
-            print latter_url
+        for latter_url in response.xpath('//ul[@class="browser alpha"]/li/a/@href').extract():
             if 'authors' in latter_url:
                 full_url = response.urljoin(latter_url)
-                time.sleep(10)
-                yield Request(full_url, self.extract_index)
+                yield Request(full_url, callback=self.extract_link_to_doc)
+                break
 
-    # def extract_index(self, response):
-    #     # authors = []
-    #     # for author in response.xpath('//h2/a/text()').extract():
-    #     #     if u'\xb6' not in author:
-    #     #         print author
-    #     for h2 in response.xpath('//h2/following-sibling::ul/li[@class="pgdbetext"]'):
-    #         lang = h2.xpath('./text()').extract()
-    #         if lang and 'English' in lang[0]:
-    #             doc_name = h2.xpath('./a/text()').extract()
-    #             doc_path = h2.xpath('./a/@href').extract()
-    #             full_url = response.urljoin(doc_path[0])
-    #             time.sleep(10)
-    #             yield Request(full_url, self.test(response, doc_name))
-    #
-    # def test(self, response, name):
-    #     print name
-    #
+    def extract_link_to_doc(self, response):
+        for latter_url in response.xpath('//li/a/@href').extract():
+            if 'etext' in latter_url:
+                full_url = response.urljoin(latter_url)
+                yield Request(full_url, callback=self.extract_document)
+                break
+
+    def extract_document(self, response):
+
+        book = BookItem()
+
+        for table_cell in response.xpath('//tr'):
+            header = table_cell.xpath('th/text()').extract()
+
+            if len(header) == 0:
+                book['zip_link'] = response.urljoin(table_cell.xpath('td[2]/a/@href').extract()[0])
+                continue
+
+            if header[0] == 'Author':
+                book['author'] = table_cell.xpath('td/text()').extract()
+            elif header[0] == 'Title':
+                book['title'] = table_cell.xpath('td/text()').extract()
+            elif header[0] == 'Language':
+                book['lang'] = table_cell.xpath('td/text()').extract()
+            elif header[0] == 'LoC Class':
+                book['loc_class'] = table_cell.xpath('td/text()').extract()
+            elif header[0] == 'Release Date':
+                book['rdate'] = table_cell.xpath('td/text()').extract()
+            else:
+                continue
+
+        return book
+
+
     # def extract_document(self, response):
     #     book = BookItem()
     #     content = []
@@ -116,6 +133,9 @@ process = CrawlerProcess({
     #   it is possible to crawl the website without any restrictions
     'USER_AGENT': 'googlebot',
     'DOWNLOADER_CLIENTCONTEXTFACTORY': 'contextfactory.MyClientContextFactory',
+    'DOWNLOAD_HANDLERS': {
+        's3': None,
+    },
     # 'ITEM_PIPELINES': {
     #     'bookitempipeline.BookItemPipeline': 10
     # }
