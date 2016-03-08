59933fc3f839bc901cf0cc4f1bb6cc7726aff6ad
dickson <dicksonchow2007@gmail.com>
2016-03-09 01:40:11 +0800

    pipeline dump data to database

--- data_etl/pipe_raw_data.py
+++ data_etl/pipe_raw_data.py
@@ -1,11 +1,86 @@
+import sys
 import zipfile
+from database import connect_to_database
+from data_etl.db_schema_classes.author import Author
+from data_etl.db_schema_classes.document import Document
 
 dir_path = '/tmp/gutenberg/'
-path_to_store_txt = '/tmp/gutenberg/txt'
+path_to_store_txt = '/tmp/gutenberg/txt/'
 
 
 def process_book_item(book):
+    """
+        This function will catch the pipeline from Gutenberg
+        crawler, precisely, the book item/object and continue
+        to dump the data into the database.
+
+        The following code will store the meta-data of the
+        book item. After that, the zip file will be extracted
+        by read_file_get_content function in order to get its
+        content.
+    """
+
+    SQL_INSERT_QUERY = ''
     zip_path = dir_path + book['host_path'][0]
+
+    author = Author(book['author'][0])
+    author_queried_id = connect_to_database.test_if_author_exists(author)
+
+    """
+        The test_if_author_exists function returns -1
+        if the name of the author is not found on the database.
+
+        Visit connect_to_database.py for more details.
+    """
+    if author_queried_id is -1:
+        SQL_INSERT_QUERY += author.get_author_insert_query()
+
     with zipfile.ZipFile(zip_path, 'r') as z:
         z.extractall(path_to_store_txt)
-        print z.namelist()[0]
+        """
+            There is a checking in the Document class to
+            see if author_queried_id is 0, which indicates
+            the author was not found in the database
+
+            In this case, the script will first insert the
+            info of that author into the database. Then,
+            the document will use that newly generated author_id
+            to do its job.
+
+            Otherwise, the script will just use the author_id
+            returned by the connect_to_database.test_if_author_exists(author)
+            function.
+        """
+        SQL_INSERT_QUERY += Document(-1, author_queried_id, book['title'][0], read_file_get_content(z.namelist()[0]))\
+                            .get_doc_insert_query()
+        print SQL_INSERT_QUERY
+
+
+def read_file_get_content(file_path):
+    """
+        This function will read the file stored on the computer
+        and then return the contents in it.
+
+        To begin with, the function will set the default encodeing
+        to utf-8.
+    """
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+
+    with open(path_to_store_txt + file_path, 'r') as doc_file:
+
+        contin = False
+        content = ''
+
+        for line in doc_file.readlines():
+            if '*** START OF THE PROJECT GUTENBERG' in line:
+                contin = True
+                continue
+
+            if '*** END OF THE PROJECT GUTENBERG' in line:
+                break
+
+            if contin:
+                content += line
+
+    return content
--- data_etl/raw_data_population.py
+++ data_etl/raw_data_population.py
@@ -10,6 +10,16 @@ base_dir = '/home/dickson/Documents/programming/Gutenberg/txt/'
 min_size = 100 * 1000
 
 """
+    This script was originaly made in order to process raw data
+    stored on the computer and dump them into the database.
+
+    However, this is no longer needed because we have developed
+    a crawler to scrape data from the internet. Still, we keep
+    this file for a reference because the pipeline script needs
+    some functions that are used in this script.
+"""
+
+"""
     To make python read all the files with
     utf-8 encoding by default
 """
@@ -31,7 +41,7 @@ for root, sub_dir, files in os.walk(base_dir):
             with open(root + fi, 'r') as doc_file:
                 book['content'] = doc_file.read()
 
-            author = Author(book['author_name'])
+            author = Author(book['author'])
             author_queried_id = connect_to_database.test_if_author_exists(author)
 
             """
@@ -43,6 +53,19 @@ for root, sub_dir, files in os.walk(base_dir):
             if author_queried_id is -1:
                 SQL_INSERT_QUERY += author.get_author_insert_query()
 
+            """
+                There is a checking in the Document class to
+                see if author_queried_id is 0, which indicates
+                the author was not found in the database
+
+                In this case, the script will first insert the
+                info of that author into the database. Then,
+                the document will use that newly generated author_id
+                to do its job.
+
+                Otherwise, the script will just use the author_id
+                returned by the connect_to_database.test_if_author_exists(author)
+                function.
+            """
             SQL_INSERT_QUERY += Document(-1, author_queried_id, book['title'], book['content']).get_doc_insert_query()
-            print fi
             connect_to_database.execute_insert_query(SQL_INSERT_QUERY)
