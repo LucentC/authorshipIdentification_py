7a383e14b18a13c462a8bce6f3fffc5368003a00
Dickson <dicksonchow2007@gmail.com>
2016-06-08 02:25:45 +0800

    add bigram to char bigrams

---
+++ experiment/bigrams_to_char_bigrams.py
@@ -0,0 +1,17 @@
+import itertools
+from database import connect_to_database
+
+SQL_SELECT_QUERY = 'SELECT bigram_id, doc_id, para_id, bigram FROM bigram_feature ORDER BY bigram_id LIMIT 100;'
+
+word_list = list(itertools.chain.from_iterable([[word for word in item['bigram'].split('-')]
+             for item in connect_to_database.execute_select_query(SQL_SELECT_QUERY)]))
+
+previous = ''
+for word in word_list:
+
+    if previous == word:
+        continue
+    previous = word
+
+    for idx in range(0, len(word) - 1):
+        print word[idx], ' - ', word[idx + 1]
