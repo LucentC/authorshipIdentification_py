14716bc7d320b34c07308ed2b77356af1f905bcd
songponssw <songpon.s@ku.th>
2019-08-24 22:42:44 +0700

    change .slq name

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 IMGNAME=stylometry_web
+NAME=stylometry_web
 LOCALPORT=16112
-VOL="-v /postgres/demo/stylometry_demo.sql:/postgres/stylometry_demo.sql"
+VOL="-v /postgres/demo/stylometry_demo.sql:/postgres/stylometry.sql"
 
 if [[ "$(docker images -q $IMGNAME 2> /dev/null)" == "" ]]; then
   docker build -t $IMGNAME .;
 fi
 
 echo $VOL
-docker run -d --restart=always -e FLASK_DEBUG=1 -p $LOCALPORT:80 $VOL $IMGNAME bash -c \
+docker run --name $NAME -d --restart=always -e FLASK_DEBUG=1 -p $LOCALPORT:80 $VOL $IMGNAME bash -c \
   "service postgresql start && \
   ./preconfigure.sh ipost && \
   su stylometry -c \"psql -d stylometry -f /postgres/stylometry.sql\" && \
