849da0677473e24eb883e49f3853fa18b708bf2b
songponssw <songpon.s@ku.th>
2019-08-24 21:54:19 +0700

    revert bf edit static path

--- run.sh
+++ run.sh
@@ -2,19 +2,18 @@
 
 IMGNAME=stylometry_web
 LOCALPORT=16112
-NAME=stylometry_web
-VOL="-v /postgres/demo/stylometry_demo.sql:/postgres/stylometry.sql"
+VOL="-v /pgdata/:/var/lib/postgresql/9.5/main -v /postgres/demo/stylometry_final.sql:/postgres/stylometry_demo.sql"
 
 if [[ "$(docker images -q $IMGNAME 2> /dev/null)" == "" ]]; then
   docker build -t $IMGNAME .;
 fi
 
 echo $VOL
-docker run --name $NAME -d --restart=always -e FLASK_DEBUG=1 -p $LOCALPORT:80 $VOL $IMGNAME bash -c \
+docker run -d --restart=always -e FLASK_DEBUG=1 -p $LOCALPORT:80 $VOL $IMGNAME bash -c \
   "service postgresql start && \
   ./preconfigure.sh ipost && \
   su stylometry -c \"psql -d stylometry -f /postgres/stylometry.sql\" && \
-  python -m flask run -h -0.0.0.0 -p 80"
+  python -m flask run -p 80 -h 0.0.0.0"
 
 # restore data
 # su stylometry -c "psql -d stylometry -f /postgres/stylometry.sql"
